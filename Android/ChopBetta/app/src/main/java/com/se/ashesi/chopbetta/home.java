7c5f4b550f617e39788e350da0760b4842a73b2d
shamir adjaku <eyramadjaku@gmail.com>
2014-09-24 15:41:53 +0000

    enabled http connection for list

--- Android/ChopBetta/app/src/main/java/com/se/ashesi/chopbetta/home.java
+++ Android/ChopBetta/app/src/main/java/com/se/ashesi/chopbetta/home.java
@@ -1,6 +1,7 @@
 package com.se.ashesi.chopbetta;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -8,6 +9,13 @@ import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -21,16 +29,28 @@ public class home extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        //make call to get list
+        DownloadWebPageTask task = new DownloadWebPageTask();
+        System.out.println("About to execute task");
+        task.execute(new String[]  { "http://10.10.30.80/test.php" }); //url must always be changed
+        //parse json array to a string array
+
+
+    }
+
+    /**
+     *
+     * @param result
+     */
+    public void fillList(String result){
         final ListView listView =(ListView) findViewById(R.id.listView);
-        String [] values = new String[]{"one","two","three","four","five","six","seven","eight","nine"};
+        String [] values = new String[]{result};
 
         final ArrayList<String> list = new ArrayList<String>();
         Collections.addAll(list, values);
         final StableArrayAdapter adapter = new StableArrayAdapter(this,android.R.layout.simple_list_item_1, list);
         listView.setAdapter(adapter);
-
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -39,17 +59,21 @@ public class home extends ActionBarActivity {
         return true;
     }
 
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
     }
+
+    /**
+     * List Array Adapter
+     * currently handles only strings
+     *
+     */
 
     private class StableArrayAdapter extends ArrayAdapter<String> {
 
@@ -75,4 +99,42 @@ public class home extends ActionBarActivity {
 
     }
 
+    /** AsyncTask class works perfectly ... don't touch
+     create an object and call the object.execute(string array with the url)
+     handle the response in onPostExecute() returns a string
+     **/
+    private class DownloadWebPageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            String response = "";
+            for (String url : urls) {
+                DefaultHttpClient client = new DefaultHttpClient();
+                HttpGet httpGet = new HttpGet(url);
+                try {
+                    HttpResponse execute = client.execute(httpGet);
+                    InputStream content = execute.getEntity().getContent();
+
+                    BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                    String s = "";
+                    while ((s = buffer.readLine()) != null) {
+                        response += s;
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            //    textView.setText(result);
+            fillList(result);
+        }
+    }
+
 }
+
+
+
