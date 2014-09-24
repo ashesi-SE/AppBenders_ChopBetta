c95107a318220131cfbd8cd887e8f07a336302cf
shamir adjaku <eyramadjaku@gmail.com>
2014-09-24 15:24:31 +0000

    added a listview and a string list adapter

--- Android/ChopBetta/app/src/main/java/com/se/ashesi/chopbetta/home.java
+++ Android/ChopBetta/app/src/main/java/com/se/ashesi/chopbetta/home.java
@@ -1,9 +1,17 @@
 package com.se.ashesi.chopbetta;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class home extends ActionBarActivity {
@@ -12,6 +20,15 @@ public class home extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        final ListView listView =(ListView) findViewById(R.id.listView);
+        String [] values = new String[]{"one","two","three","four","five","six","seven","eight","nine"};
+
+        final ArrayList<String> list = new ArrayList<String>();
+        Collections.addAll(list, values);
+        final StableArrayAdapter adapter = new StableArrayAdapter(this,android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+
     }
 
 
@@ -33,4 +50,29 @@ public class home extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private class StableArrayAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+    }
+
 }
