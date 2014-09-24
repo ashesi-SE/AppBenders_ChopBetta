6cc011a17e0c5af191d6c7622d5940bfee865595
shamir adjaku <eyramadjaku@gmail.com>
2014-09-24 19:03:28 +0000

    bug from ref#1 fixed.Can now parse json response.

--- Android/ChopBetta/app/src/main/java/com/se/ashesi/chopbetta/home.java
+++ Android/ChopBetta/app/src/main/java/com/se/ashesi/chopbetta/home.java
@@ -12,6 +12,9 @@ import android.widget.ListView;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -32,24 +35,58 @@ public class home extends ActionBarActivity {
         //make call to get list
         DownloadWebPageTask task = new DownloadWebPageTask();
         System.out.println("About to execute task");
-        task.execute(new String[]  { "http://10.10.30.80/test.php" }); //url must always be changed
+        task.execute(new String[]{"http://10.10.29.67/test.php"}); //url must always be changed
         //parse json array to a string array
 
 
     }
 
     /**
-     *
+     *Fill list view with items from the server
      * @param result
      */
     public void fillList(String result){
         final ListView listView =(ListView) findViewById(R.id.listView);
-        String [] values = new String[]{result};
+        String [] values =parseResponse(result);
 
         final ArrayList<String> list = new ArrayList<String>();
         Collections.addAll(list, values);
         final StableArrayAdapter adapter = new StableArrayAdapter(this,android.R.layout.simple_list_item_1, list);
         listView.setAdapter(adapter);
+    }
+
+/*
+parses the json response from the server into a string array
+ */
+    public String[] parseResponse(String result){
+
+//        String resultArray
+        ArrayList<String> menuArray = new ArrayList<String>();
+        try {
+            JSONArray aJsonArray = new JSONArray(result);
+            for (int i=0; i<aJsonArray.length();i++){
+                JSONObject aJsonObject = aJsonArray.getJSONObject(i);
+                menuArray.add(aJsonObject.getString("menu_item"));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return objectArrayToStringArray(menuArray.toArray());
+    }
+
+    public String[] objectArrayToStringArray(Object[] result){
+        int size = result.length;
+        String[] toReturn;
+        if(size>0) {
+            toReturn = new String[size];
+            for (int i = 0; i < size; i++) {
+                toReturn[i] = (String) result[i];
+            }
+        }
+        else{
+            toReturn = new String []{"No items"};
+        }
+        return toReturn;
     }
 
     @Override
