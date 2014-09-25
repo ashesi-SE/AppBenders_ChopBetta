204f6ba7df42242e14c6444cccb3b60979cc01b5
vanderock1 <vanderock1@gmail.com>
2014-09-25 08:33:17 +0000

    stable iter1

--- Web/ChopBetta/index.php
+++ Web/ChopBetta/index.php
@@ -1,4 +1,4 @@
-<<!doctype html>
+<!doctype html>
 <html>
 <head>
     <link rel="stylesheet" href="assets/css/style.css"/>
@@ -16,9 +16,9 @@
         </ul>
         <ul class="right">
             <li class="has-dropdown">
-                <a href="\">{{Loginstate}}</a>
+                <a href="/">Kingston Coker</a>
                 <ul class="dropdown">
-                    <li class="active" ><button ng-click="loginfb()" >{{LogAction}}</button></li>
+                    <li class="active" ><button ng-click="loginfb()" >Logout</button></li>
                 </ul>
             </li>
         </ul>
@@ -26,57 +26,59 @@
 </nav>
 
 <main class="centerPage">
-    <section>
-
-<div id="today_menu_modal" class="reveal-modal small" data-reveal>
-    <div class="small-3 columns">
-          <label for="right-label" class="right">Today's meal</label>
-    </div>
-        <div class="small-9 columns">
-          <input type="text" id="right-label" placeholder="Inline Text Input">
-        </div>
-  <a class="close-reveal-modal">&#215;</a>
-</div>
-
-<div id="create_meal_modal" class="reveal-modal small" data-reveal>
-    <div class="small-3 columns">
-          <label for="right-label" class="right">Meal</label>
-    </div>
-        <div class="small-9 columns">
-          <input type="text" id="right-label" placeholder="Inline Text Input">
-        </div>
-    <a class="close-reveal-modal">&#215;</a>
-</div>
-
-<div id="add_foodItem_modal" class="reveal-modal small" data-reveal>
-    <div class="small-3 columns">
-          <label for="right-label" class="right">Food item</label>
-    </div>
-        <div class="small-9 columns">
-          <input type="text" id="right-label" placeholder="Inline Text Input">
-        </div>
-    <a class="close-reveal-modal">&#215;</a>
-</div>
-
+    <section id="dataRows">
         <div class="row" id="addMealRow1">
             <label for="meals">Create menu</label>
             <div class="large-9 columns" style="padding-left: 0">
 
                     <select class="meals">
-                        <option value="husker">Husker</option>
-                        <option value="starbuck">Starbuck</option>
-                        <option value="hotdog">Hot Dog</option>
-                        <option value="apollo">Apollo</option>
+
                     </select>
 
             </div>
             <div class="large-3 columns addBtn" style="padding-left: 0">
-                 <button class="menuControl" >Add to menu</button>
+                 <button class="menuControl" onclick="addMealRow(this)" >Add to menu</button>
             </div>
         </div>
         <div id="adder"></div>
 
     </section>
+    <div id="today_menu_modal" class="reveal-modal small" data-reveal>
+        <div class="small-3 columns">
+            <label for="right-label" class="right">Today's meal</label>
+        </div>
+        <div class="small-9 columns">
+            <input type="text" id="right-label" placeholder="Inline Text Input">
+        </div>
+        <a class="close-reveal-modal">&#215;</a>
+    </div>
+
+    <div id="create_meal_modal" class="reveal-modal small" data-reveal>
+        <div class="small-3 columns">
+            <label for="right-label" class="right">Meal</label>
+        </div>
+        <div class="small-9 columns">
+            <input type="text" id="right-label" placeholder="Inline Text Input">
+        </div>
+        <a class="close-reveal-modal">&#215;</a>
+    </div>
+
+    <div id="add_foodItem_modal" class="reveal-modal small" data-reveal>
+
+        <div class="row">
+            <div class="large-12 columns">
+                <div class="row collapse">
+                    <div class="small-10 columns">
+                        <input id="foodItem_input" type="text" placeholder="Jollof, veg and chicken...">
+                    </div>
+                    <div class="small-2 columns">
+                        <a href="#" class="button postfix" onclick="addMealdiff(this)">Save</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a class="close-reveal-modal">&times;</a>
+    </div>
 </main>
 </body>
 <script src="assets/js/jquery-1.11.0.js" type="text/javascript"></script>
